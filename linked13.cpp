1ce9f431ed11239c9cc5c512e95900e79d58affe
rogan-macleod <roganemacleod@gmail.com>
2025-01-13 18:15:06 +0000

    test

---
+++ linked13.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+
+class Node{
+public:
+	int value;
+	Node* next;
+
+
+};
+
+
+void insert(Node** head,int newval){
+
+	Node* newnode = new Node();
+	newnode->value = newval;
+	newnode->next = *head;
+	*head = newnode;
+
+}
+
+void append(Node** head,int newval){
+
+	Node* newnode = new Node();
+	newnode->value = newval;
+	newnode->next = nullptr;
+
+	if (*head == nullptr){
+
+		*head = newnode;
+		return;
+	}
+	Node* current = *head;
+	while(current->next != nullptr){
+
+		current = current->next;
+	}
+	current->next = newnode;
+
+
+
+}
+
+
+void printlist(Node* n){
+
+	while(n!=nullptr){
+
+		std::cout << n->value << "\n";
+		n = n->next;
+	}
+
+}
+
+
+void deletelist(Node* head){
+
+	Node* current = head;
+	Node* nextnode;
+	while(current != nullptr){
+
+		nextnode = current->next;
+		delete current;
+		current = nextnode;
+	}
+
+}
+
+
+int main(){
+
+	Node* head = nullptr;
+
+	insert(&head,1);
+	insert(&head,2);
+	insert(&head,3);
+	insert(&head,4);
+	insert(&head,5);
+	insert(&head,6);
+
+	append(&head, 17);
+
+	printlist(head);
+
+	deletelist(head);
+
+	head = nullptr;
+
+
+
+	return 0;
+}
